cc86d4c8493bd30022eb051fe48bbf6e9e029706
guestccc <1009078602@qq.com>
2020-04-02 13:52:00 +0800

    :art: frist commit

---
+++ .vuepress/config.js
@@ -0,0 +1,74 @@
+module.exports = {
+  title: '过客',
+  description: 'Just playing around',
+  base:'/blog/', // 如果你想将你的网站部署到 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/"
+  head: [
+    ['link', { rel: 'icon', href: '/assets/img/LOGO.png' }]
+  ],
+  themeConfig: {
+    logo: '/assets/img/LOGO.png',
+    sidebar: 'auto',
+    nav: [
+      {
+        text: '前端架构之路',
+        ariaLabel: '前端架构之路',
+        items: [
+          { 
+            text: 'javaScript基础', 
+            items: [
+              { text: '基础知识', link: '/guide/' },
+              { text: '函数', link: 'https://google.com' },
+              { text: 'es6', link: 'https://google.com' },
+              { text: '异步编程', link: 'https://google.com' },
+              { text: '设计模式', link: 'https://google.com' },
+            ]
+          },
+          { 
+            text: '计算机网络',
+            items: [
+              { text: '网络协议', link: 'https://google.com' },
+              { text: '网络请求', link: 'https://google.com' },
+              { text: '网络安全', link: 'https://google.com' },
+              { text: '模块加载', link: 'https://google.com' },
+            ]
+          },
+          { 
+            text: 'Vue.js', 
+            items: [
+              { text: 'Vue.js', link: 'https://google.com' },
+              { text: 'Nuxt.js', link: 'https://google.com' },
+              { text: 'Vue源码解读', link: 'https://google.com' },
+              { text: '组件架构', link: 'https://google.com' },
+            ]
+          },
+          { text: 'React.js', link: 'https://google.com' },
+          { 
+            text: 'node.js', 
+            items: [
+              { text: '基础', link: 'https://google.com' },
+              { text: 'web服务express', link: 'https://google.com' },
+            ]
+          },
+          { 
+            text: '前端工程化', 
+            items: [
+              { text: 'webpack', link: 'https://google.com' },
+              { text: 'babel', link: 'https://google.com' },
+              { text: 'CI/CD', link: 'https://google.com' },
+              { text: '质量检测', link: 'https://google.com' },
+              { text: '编码规范', link: 'https://google.com' },
+            ]
+          },
+        ]
+      },
+      { 
+        text: '面试', 
+        items: [
+          { text: 'js基础', link: '/面试/js基础/' },
+        ]
+      },
+      { text: '随笔', link: 'https://google.com' },
+      { text: 'GitHub', link: 'https://github.com/guestccc' },
+    ]
+  }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+---
+home: true
+heroImage: /blog/assets/img/LOGO.png
+heroText: 过客（前端）
+tagline: 个人技术提升的知识体系，终身学习的信仰
+actionText: 欢迎指点 →
+actionLink: /guide/
+features:
+- title: 前端架构
+  details: 个人前端架构学习之路
+- title: 随笔
+  details: 日常项目小坑，小白笔记
+# - title: 高性能
+#   details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
+footer: MIT Licensed | Copyright © 2018-present Evan You
+---
+
+<!-- <img :src="$withBase('/blog/assets/img/LOGO.png')" alt="foo"> -->
